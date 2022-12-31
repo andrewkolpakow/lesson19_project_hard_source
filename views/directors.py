fcd7fafecaf9e76a8ff71edf194000501f8b2036
Andrew <andrew.vk90@gmail.com>
2022-12-31 20:03:16 +0300

    Complete

--- views/directors.py
+++ views/directors.py
@@ -3,17 +3,21 @@ from flask_restx import Resource, Namespace
 
 from dao.model.director import DirectorSchema
 from implemented import director_service
+from decorators import auth_required, admin_required
 
 director_ns = Namespace('directors')
 
 
 @director_ns.route('/')
 class DirectorsView(Resource):
+
+    @auth_required
     def get(self):
         rs = director_service.get_all()
         res = DirectorSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_required
     def post(self):
         request_json = request.json
         director = director_service.create(request_json)
@@ -22,11 +26,14 @@ class DirectorsView(Resource):
 
 @director_ns.route('/<int:rid>')
 class DirectorView(Resource):
+
+    @auth_required
     def get(self, rid):
         r = director_service.get_one(rid)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
 
+    @admin_required
     def put(self, rid):
         request_json = request.json
         if "id" not in request_json:
@@ -35,6 +42,7 @@ class DirectorView(Resource):
         director_service.update(request_json)
         return "", 204
 
+    @admin_required
     def delete(self,rid):
         director_service.delete(rid)
         return "", 204
